2551d3b8a5bcb6cebc7b3e339ccf786804776f74
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-10-20 11:58:38 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=10
+y="charvi"
+print(x)
+
+
+# In[2]:
+
+
+z=float(5)
+print(z)
+
+
+# In[4]:
+
+
+x="charvi"
+y=3.0
+print(type(x))
+print(type(y))
+
+
+# In[5]:
+
+
+myVariable='2'
+print(myVariable)
+
+
+# In[6]:
+
+
+MyName='smith'
+print(MyName)
+
+
+# In[7]:
+
+
+x,y,z='Orange' , 'Apple' , 'plum'
+print(x)
+print(y)
+print(z)
+
+
+# In[8]:
+
+
+x=y=z="python"
+print(x)
+print(y)
+print(z)
+
+
+# In[9]:
+
+
+x="python"
+y="is"
+z="awesome"
+print(x,y,z)
+
+
+# In[10]:
+
+
+x="python"
+y="is"
+z="awesome"
+print(x + y + z)
+
+
+# In[12]:
+
+
+x=3
+y=10
+z=5
+print(x + y + z)
+
+
+# In[13]:
+
+
+x=3
+y=10
+z=x+y
+print(z)
+
+
+# In[14]:
+
+
+s="multiple string"
+print(s)
+
+
+# In[15]:
+
+
+list1=[1,"hi","python",2]
+print(2type(list1))
+print(list1[0:2])
+
+
+# In[17]:
+
+
+list1=[1,"hi","python",2]
+print(type(list1))
+print(list1[0:4])
+
+
+# In[18]:
+
+
+tup=("hi","python",2)
+print(tup)
+
+
+# In[20]:
+
+
+d={1:'Jimmy',2:'Alex',3:'john',4:'mikki'}
+print("1st name is "+d[1])
+print(d.keys())
+print(d.values())
+
+
+# In[22]:
+
+
+print(type(True))
+print(type(False))
+
+
+# In[23]:
+
+
+print(type(1))
+print(type(0))
+
+
+# In[26]:
+
+
+set2={'james',2,3,'python'}
+print(set2)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ operators.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+x=int(input('enter 1st number '))
+y=int(input('enter 2nd number '))
+print(x+y)
+
+
+# In[ ]:
+
+
+x=int(input('enter 1st number '))
+y=int(input('enter 2nd number '))
+print(x-y)
+
+
+# In[4]:
+
+
+x=int(input('enter 1st number '))
+y=int(input('enter 2nd number '))
+print(x*y)
+
+
+# In[5]:
+
+
+x=int(input('enter 1st number '))
+y=int(input('enter 2nd number '))
+print(x/y)
+
+
+# In[6]:
+
+
+x=int(input('enter 1st number '))
+y=int(input('enter 2nd number '))
+print(x%y)
+
+
+# In[9]:
+
+
+#simple interest
+principal=float(input('enter principal amount '))
+rate=float(input('enter rate '))
+time=float(input('enter time period '))
+s_i=(principal*rate*time)/100
+print('the simple interest ' ,s_i ,"RS." )
+
+
+# In[12]:
+
+
+#area of rectangle
+l=int(input("enter the length "))
+b=int(input("enter the breadth "))
+a=l*b
+print("area of rectangle is " , a ,' m.sq.')
+
+
+# In[ ]:
+
+
+#area of sqaure
+side=int(input("enter the side "))
+a=side*side
+print("area of square is " , a ,' m.sq.')
+
+
+# In[14]:
+
+
+#all operations 
+x=int(input('enter 1st number '))
+y=int(input('enter 2nd number '))
+print('the addition is ' ,x+y)
+print('the subtraction is ' ,x-y)
+print('the multiplication is ' ,x*y)
+print('the division is ' , x/y)
+print('the modulus is ', x%y)
+
+
+# In[ ]:
+
+
+
+
