11c49f9a996eec82243666628a183c549fd3f368
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-10-20 11:57:38 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name=input("enter your name:")
+print("hello," + name)
+
+
+# In[2]:
+
+
+number=int(input("enter a number:"))
+print(number+ 200)
+
+
+# In[3]:
+
+
+name=(input("enter a name:"))
+print(name)
+print(type(name))
+
+
+# In[4]:
+
+
+number=(input("enter a number:"))
+print(number)
+print(type(number))
+
+
+# In[5]:
+
+
+print(1,2,3,5)
+
+
+# In[6]:
+
+
+print(1,3,5,7,sep='*')
+
+
+# In[8]:
+
+
+print(1,3,5,7,sep='*',end='\n',8,9)
+
+
+# In[9]:
+
+
+print(1,3,5,7,sep='*',end='%')
+
+
+# In[10]:
+
+
+x=5;
+y=19
+print('the vale of x is {} and y is {}'.format(x,y))
+
+
+# In[11]:
+
+
+print('i love {0} and {1}'.format('chowmien','manchurian'))
+
+
+# In[12]:
+
+
+print("hello {name}, {greeting}".format(greeting="good afternoon",
+                                       name="charvi"))
+
+
+# In[14]:
+
+
+x=input("enter your name");
+y=input("enter the greeting")
+print('hello {} , {}'.format(x,y))
+
+
+# In[18]:
+
+
+number=int(input("enter a number:"))
+print('the integer is ' , number)
+
+
+# In[16]:
+
+
+number=int(input("enter a integer:"))
+print(number)
+
+
+# In[17]:
+
+
+number=float(input("enter a float:"))
+print(number)
+
+
+# In[ ]:
+
+
+
+
